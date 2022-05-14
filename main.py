42bff6360e7adf311454768dbb7d545b3e1ae8c7
Komarov-Tch <komarovegr@gmail.com>
2022-05-14 15:11:01 +0700

    Поправил задание, сделал адреса относительными. Раскидал текст по папкам.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+
 
 def read_reciept(name):
     # Создаем словарь из файла
@@ -46,25 +48,26 @@ def get_shop_list_by_dishes(dishes, person_count):
 
 
 def add_files(lst_files):
-    # в функцию передаем список файлов, которые нужно объединить
-    # универсальное решение, под n-oe количество файлов.
-    # открываем каждый файл, считаем количество строк,
-    # создаем пары (название файла, количество строк), сортируем по количеству строк.
-    # В порядке сортировки открываем файлы  и считываем текст, записываем в новый файл
+    # Задание 3
     work_zone = []
     for i in lst_files:
-        with open(i, mode='r', encoding='utf-8') as f:
+        with open(os.path.join(text_direct, i), mode='r', encoding='utf-8') as f:
             work_zone.append((i, len(f.readlines())))
     work_zone.sort(key=lambda x: x[1])
     f = open('result.txt', 'w', encoding='utf-8')
     for name, lenght in work_zone:
-        with open(name, mode='r', encoding='utf-8') as work_file:
+        with open(os.path.join(text_direct, name), mode='r', encoding='utf-8') as work_file:
             print(name + '\n' + str(lenght), file=f)
             for line in work_file:
                 print(line.rstrip(), file=f)
     f.close()
 
-# cook_book = read_reciept('rec2')
-#add_files(['1.txt', '2.txt', '3.txt'])
-# print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
-# rec_reciept(cook_book, 'rec2')
+
+reciept_direct = os.path.join(os.getcwd(), 'reciepts')  # Адрес папки для рецептов
+text_direct = os.path.join(os.getcwd(), 'texts')  # Адрес папки для текстов
+lst_files = os.listdir(text_direct)  # Список файлов для склейки в 1
+# add_files(lst_files)  # Вызов функции для склейки файлов
+
+# cook_book = read_reciept(os.path.join(reciept_direct, 'rec2')) # создание словаря из текста в файле
+# print(get_shop_list_by_dishes(['Омлет', 'Омлет'], 1)) # сколько продуктов купить
+# rec_reciept(cook_book, 'rec2') # на всякий пожарный, создание файла из словаря
